9927bc28397ee8284f4d262e6519e86ccd152b03
searchsakib <searchsakib@gmail.com>
2024-10-20 04:06:50 +0600

    right and wrong words showing dynamic colors

--- app/components/TypingTest/TypingTest.tsx
+++ app/components/TypingTest/TypingTest.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { KeyboardEvent, useState } from "react";
 import Mainheader from "../ui/Mainheader";
+// import { nanoid } from "nanoid";
 
 const TypingTest = () => {
-  const paragraph = "Table";
+  const paragraph =
+    "Table orange bicycle computer giraffe toaster window socks ceiling banana cloud pencil umbrella sandwich chair ocean telephone cactus jelly carpet spaceship notebook waterfall tree dragon shoes monitor pizza lamp river guitar blanket mountain cup hat airplane dolphin brick mirror grass paper";
+
   const [inputValue, setInputValue] = useState("");
 
-  let green = "";
-  if (paragraph === inputValue) {
-    green = "text-green-400";
-  }
+  const eachCharacterOfParagraph = paragraph.split("");
+  const eachCharacterOfInputValue = inputValue.split("");
 
-  console.log(inputValue);
+  const handleClick = (e: KeyboardEvent) => {
+    e.preventDefault();
+    if (e.code === "Space") {
+      // const inputElement = e.target as HTMLInputElement;
+    }
+    console.log(e);
+  };
 
   return (
     <div>
@@ -18,23 +25,41 @@ const TypingTest = () => {
         <Mainheader>Typing Test</Mainheader>
       </div>
       <div
-        className={`pt-5 w-1/2 text-center mx-auto text-3xl leading-relaxed ${green}`}
+        className={`pt-5 w-1/2 mx-auto text-3xl leading-relaxed flex justify-center`}
       >
         <p>
-          {paragraph}
-          {/* Table orange bicycle computer giraffe toaster window socks ceiling
-          banana cloud pencil umbrella sandwich chair ocean telephone cactus
-          jelly carpet spaceship notebook waterfall tree dragon shoes monitor
-          pizza lamp river guitar blanket mountain cup hat airplane dolphin
-          brick mirror grass paper */}
+          {eachCharacterOfParagraph.map(
+            (singleCharacterFromParagraph, index) => {
+              let eachCharacterColor = "";
+              if (
+                eachCharacterOfInputValue[index] ===
+                singleCharacterFromParagraph
+              ) {
+                eachCharacterColor = "text-green-300";
+              } else if (eachCharacterOfInputValue[index] !== undefined) {
+                eachCharacterColor = "text-red-300";
+              }
+
+              return (
+                <span key={index} className={`${eachCharacterColor}`}>
+                  {singleCharacterFromParagraph}
+                </span>
+              );
+            }
+          )}
         </p>
       </div>
       <div className="pt-10 text-center mx-auto text-3xl leading-relaxed">
         <input
+          className=""
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          onKeyUp={handleClick}
         />
+      </div>
+      <div className="">
+        <p> {inputValue} </p>
       </div>
     </div>
   );
